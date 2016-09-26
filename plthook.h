993ee4755e0ff1450a52d86fbfbdd23f58b9214c
baldurk <baldurk@baldurk.org>
2016-09-26 16:33:38 +0200

    Add extern "C" to allow header inclusion from C++

--- plthook.h
+++ plthook.h
@@ -46,11 +46,19 @@
 
 typedef struct plthook plthook_t;
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 int plthook_open(plthook_t **plthook_out, const char *filename);
 int plthook_open_by_address(plthook_t **plthook_out, void *address);
 int plthook_enum(plthook_t *plthook, unsigned int *pos, const char **name_out, void ***addr_out);
 int plthook_replace(plthook_t *plthook, const char *funcname, void *funcaddr, void **oldfunc);
 void plthook_close(plthook_t *plthook);
 const char *plthook_error(void);
+
+#ifdef __cplusplus
+}; /* extern "C" */
+#endif
 
 #endif
