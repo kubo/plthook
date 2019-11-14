ecb60c6b0347533942f2798b0ffbf650227b5580
Kubo Takehiro <kubo@jiubao.org>
2019-11-14 23:50:06 +0900

    Fix potential incorrect parsing of /proc/self/maps on linux.
    When a line is too long and the line after PATH_MAX bytes
    is accidentally similar format with the beginning of a line,
    plthook may read it as memory protection mode of a memory region.
    (close #24)

--- plthook_elf.c
+++ plthook_elf.c
@@ -382,6 +382,7 @@ static int get_memory_permission(void *address)
     FILE *fp;
     char buf[PATH_MAX];
     char perms[5];
+    int bol = 1;
 
     fp = fopen("/proc/self/maps", "r");
     if (fp == NULL) {
@@ -390,6 +391,21 @@ static int get_memory_permission(void *address)
     }
     while (fgets(buf, PATH_MAX, fp) != NULL) {
         unsigned long start, end;
+        int eol = (strchr(buf, '\n') != NULL);
+        if (bol) {
+            /* The fgets reads from the beginning of a line. */
+            if (!eol) {
+                /* The next fgets reads from the middle of the same line. */
+                bol = 0;
+            }
+        } else {
+            /* The fgets reads from the middle of a line. */
+            if (eol) {
+                /* The next fgets reads from the beginnig of a line. */
+                bol = 1;
+            }
+            continue;
+        }
 
         if (sscanf(buf, "%lx-%lx %4s", &start, &end, perms) != 3) {
             continue;
