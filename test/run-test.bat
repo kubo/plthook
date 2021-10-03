85dad5081b5ebdbeeea790aeef95eb60b3748dea
Kubo Takehiro <kubo@jiubao.org>
2021-10-03 21:44:14 +0900

    Migrate from Travis CI to Github Actions

--- test/run-test.bat
+++ test/run-test.bat
@@ -1,2 +1,2 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" %1
+call "c:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" %1
 nmake /f Makefile.win32 check clean DLL_CFLAGS=%2 EXE_CFLAGS=%3
